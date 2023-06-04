38a7bbc58228b06ee149e5745e9f6380735d16dc
こもれび <hobbypopkechupki@gmail.com>
2023-06-04 18:21:30 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://cossedevelopmentteam.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/DocusaurusDemo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
