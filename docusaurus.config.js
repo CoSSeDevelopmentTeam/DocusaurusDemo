38ec2c53e44e824a2ed109e9b95d08fda4095359
ヨミ <hobbypopkechupki@gmail.com>
2023-06-08 00:33:40 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,7 +136,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. For more details about copyrights, visit <a href="https://gizmodo-woods.github.io/about-us" target="_blank">here</a>.`,
       },
       prism: {
         theme: lightCodeTheme,
